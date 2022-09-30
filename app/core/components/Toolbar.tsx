c0c7143a8426e2b58bf0b0aac5ce17039548eeb9
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-30 15:49:27 +0200

    Fix: Remove unneeded nav items, add log out btn, use alpha bg

--- app/core/components/Toolbar.tsx
+++ app/core/components/Toolbar.tsx
@@ -1,19 +1,8 @@
-import { Button, ButtonGroup, Heading, HStack, Image, Spacer, Stack, Text, VStack } from "@chakra-ui/react";
-import { Link } from "@remix-run/react";
+import { Heading, HStack, Image, Spacer, Stack, Text, VStack } from "@chakra-ui/react";
+import { Form, Link } from "@remix-run/react";
 import type { CurrentUser } from "~/auth/validations";
-import logo from "../../../public/images/logo.png";
 import { CenteredView } from "./CenteredView";
-
-interface NavItem {
-  text: string;
-  href: string;
-  primary?: boolean;
-}
-
-const navItems: NavItem[] = [
-  { text: "Create Account", href: "/", primary: true },
-  { text: "Log In", href: "/login" },
-];
+import { OutlinedButton } from "./OutlinedButton";
 
 interface Props {
   currentUser: CurrentUser | undefined;
@@ -22,38 +11,30 @@ interface Props {
 export function Toolbar (props: Props) {
   const { currentUser } = props;
   return (
-    <VStack bgColor={"white"} align={"stretch"} boxShadow="md">
+    <VStack bgColor={"whiteAlpha.50"} align={"stretch"} boxShadow="md">
       <CenteredView>
         <Stack direction={{ base: "column", lg: "row" }} align="center" p={4}>
           <HStack align="center" spacing={4}>
             <Link to="/">
-              <Image objectFit='contain' height={12} src={logo} alt='Logo' />
+              <Image objectFit='contain' height={12} src="/logo.png" alt='Logo' />
             </Link>
             <Link to="/">
-              <Heading color="blue.600" size="lg" fontWeight="bold">
+              <Heading color="white" size="lg" fontWeight="bold">
                 BUG TRACKER
               </Heading>
             </Link>
           </HStack>
           <Spacer />
-          {!currentUser && (
-            <ButtonGroup>
-              {navItems.map(item => (
-                <VStack key={item.text} p={0} align="stretch">
-                  <Link prefetch="intent" to={item.href}>
-                    <Button variant={item.primary ? "solid" : "ghost"} colorScheme={"blue"}>
-                      {item.text}
-                    </Button>
-                  </Link>
-                </VStack>
-              ))}
-            </ButtonGroup>
-          )}
           {currentUser && (
             <HStack p={0} justify="flex-end" align="center">
-              <Text fontSize="md" px="4" noOfLines={1}>
-                {currentUser.fullName}
+              <Text color="white" fontSize="md" px="4" noOfLines={1}>
+                {currentUser.username}
               </Text>
+              <Form action="/logout" method="post">
+                <OutlinedButton type="submit" w="100%" colorScheme="whiteAlpha">
+                  Log Out
+                </OutlinedButton>
+              </Form>
             </HStack>
           )}
         </Stack>
