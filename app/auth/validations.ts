6114138e45cc871ec00b61d79f58a9bab11b3f0a
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-30 15:47:26 +0200

    Fix: Remove unused schema, remove toLowercase

--- app/auth/validations.ts
+++ app/auth/validations.ts
@@ -1,5 +1,4 @@
-import { z } from "zod"
-import { PositiveIntSchema } from "~/core/validations";
+import { z } from "zod";
 
 export const FullNameSchema = z
   .string()
@@ -10,94 +9,22 @@ export const UsernameSchema = z
   .string()
   .min(4)
   .max(50)
-  .transform((str) => str.toLowerCase().trim())
+  .transform((str) => str.trim())
 
 export const PasswordSchema = z
   .string()
   .min(4)
   .max(100)
-  .transform((str) => str.trim())
-
-export const CreateAccountSchema = z
-  .object({
-    fullName: FullNameSchema,
-    emailAddress: UsernameSchema,
-    password: PasswordSchema,
-    passwordConfirmation: PasswordSchema,
-    redirectTo: z.string(),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"], // set the path of the error
-  });
-
-  export const CreateAdminSchema = z
-  .object({
-    fullName: FullNameSchema,
-    emailAddress: UsernameSchema,
-    password: PasswordSchema,
-    passwordConfirmation: PasswordSchema,
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"], // set the path of the error
-  });
-
-export const EditAccountSchema = z.object({
-  fullName: FullNameSchema,
-  emailAddress: UsernameSchema,
-});
-
-export const EditAdminAccountSchema = EditAccountSchema.extend({
-  userId: PositiveIntSchema,
-});
-
-export const ForgotPasswordSchema = z.object({
-  emailAddress: UsernameSchema,
-})
-
-export const ResetPasswordSchema = z
-  .object({
-    password: PasswordSchema,
-    passwordConfirmation: PasswordSchema,
-    token: z.string(),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"], // set the path of the error
-  })
+  .transform((str) => str.trim());
 
 export const ChangePasswordSchema = z.object({
   currentPassword: z.string().min(1),
   newPassword: PasswordSchema,
 })
 
-export function parseRedirectUrl (url: string) {
-  const DEFAULT_REDIRECT = "/";
-  if (!url || typeof url !== "string") {
-    return DEFAULT_REDIRECT;
-  }
-  if (!url.startsWith("/") || url.startsWith("//")) {
-    return DEFAULT_REDIRECT;
-  }
-  const redirectableUrls = [
-    '/',
-    '/employment-types',
-    '/applications',
-    '/lenders',
-    '/apply'
-  ];
-  if (redirectableUrls.includes(url)) {
-    return url;
-  }
-  return DEFAULT_REDIRECT;
-}
-
 export type CurrentUser = {
   id: number;
-  fullName: string;
-  emailAddress: string;
-  kind: string;
+  username: string;
 }
 
 export const ADMIN = "Admin";
