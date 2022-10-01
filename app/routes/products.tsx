80ad8a19f819629768c8791b311e004364b65aa7
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-10-01 11:36:53 +0200

    Feat: Create outlet based products route

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,115 +1,37 @@
-import { Button, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import { Heading, VStack } from "@chakra-ui/react";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useCatch, useFetcher, useLoaderData, useNavigate } from "@remix-run/react";
-import { z } from "zod";
-import { ActionContextProvider } from "~/core/components/ActionContextProvider";
+import { Link, Outlet, useCatch, useLoaderData, useNavigate } from "@remix-run/react";
 import { CustomCatchBoundary } from "~/core/components/CustomCatchBoundary";
 import { CustomErrorBoundary } from "~/core/components/CustomErrorBoundary";
-import type { inferSafeParseErrors } from "~/core/validations";
 
-import type { Product } from "@prisma/client";
 import { useCallback } from "react";
-import { X } from "tabler-icons-react";
 import { CenteredView } from "~/core/components/CenteredView";
 import { Toolbar } from "~/core/components/Toolbar";
-import { prisma } from "~/db.server";
-import { ProductListItem } from "~/products/components/ProductListItem";
-import { SearchProducts } from "~/products/components/SearchProducts";
 import { requireUser } from "~/session.server";
 
-const Schema = z.object({
-  searchTerms: z.string().optional(),
-});
-
 export async function loader ({ request }: LoaderArgs) {
-  const url = new URL(request.url);
-  const searchTerms = url.searchParams.get("searchTerms") || undefined;
+  const currentUser = await requireUser(request);
 
-  const result = await Schema.safeParseAsync({ searchTerms });
-  if (!result.success) {
-    throw new Response("Invalid input, please reload the page", { status: 400 });
-  }
-  const input = result.data;
-
-  const [currentUser, products] = await Promise.all([
-    requireUser(request),
-    searchTerms ?
-      prisma.product.findMany({
-        where: {
-          OR: [
-            { name: { contains: input.searchTerms } },
-            { description: { contains: input.searchTerms } },
-          ]
-        },
-      }) :
-      prisma.product.findMany()
-  ]);
-
-  return json({ currentUser, products, fields: input });
+  return json({ currentUser });
 }
 
-type Fields = z.infer<typeof Schema>;
-type FieldErrors = inferSafeParseErrors<typeof Schema>;
-type ActionData = {
-  formError?: string
-  fields?: Fields
-  fieldErrors?: FieldErrors,
-  products: Product[]
-};
-
 export default function Index () {
-  const { currentUser, products, fields } = useLoaderData<typeof loader>();
-  const fetcher = useFetcher<ActionData>();
-
-  const relevantProducts = fetcher.data?.products || products;
-  const relevantInput = fetcher.data?.fields || fields;
-
-  const isProcessing = !!fetcher.submission;
+  const { currentUser } = useLoaderData<typeof loader>();
 
   return (
     <VStack align="stretch" spacing={0}>
       <Toolbar currentUser={currentUser} />
-      <VStack align="stretch" py={4}>
+      <VStack align="stretch" py={6}>
         <CenteredView flexGrow={1}>
-          <Heading role="heading" size="md" color="white" px={4}>
-            Products
-          </Heading>
-          <fetcher.Form method="get">
-            <ActionContextProvider
-              fields={relevantInput}
-              fieldErrors={fetcher.data?.fieldErrors}
-              formError={fetcher.data?.formError}
-              isSubmitting={isProcessing}
-            >
-              <SearchProducts isProcessing={isProcessing} />
-            </ActionContextProvider>
-          </fetcher.Form>
-          {relevantInput?.searchTerms && (
-            <HStack align="center" px={4} spacing={4}>
-              <Text fontSize="sm" color="gray.400">
-                {relevantProducts.length} result(s) for products matching <b>"{relevantInput.searchTerms}"</b>
-              </Text>
-              <fetcher.Form method="get">
-                <input name="searchTerms" type="hidden" value="" />
-                <Button type="submit" rightIcon={<X />} colorScheme='whiteAlpha' size="sm" variant='outline'>
-                  Clear
-                </Button>
-              </fetcher.Form>
-            </HStack>
-          )}
-          {!relevantProducts.length && (
-            <VStack align="stretch" p={4}>
-              <Text fontSize="md" color="gray.400">No products found</Text>
-            </VStack>
-          )}
-          {relevantProducts.length && (
-            <VStack align="stretch" p={4}>
-              {relevantProducts.map(product => (
-                <ProductListItem key={product.id} {...product} />
-              ))}
-            </VStack>
-          )}
+          <VStack align="flex-start">
+            <Link to="/products">
+              <Heading role="heading" size="md" color="white" px={4}>
+                Products
+              </Heading>
+            </Link>
+          </VStack>
+          <Outlet />
         </CenteredView>
       </VStack>
     </VStack>
